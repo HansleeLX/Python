c8131e52ee24912ca79f5d562dcd004d472189de
XIN Li <HansleeLX@users.noreply.github.com>
2018-07-26 14:33:04 +1000

    basic practice

---
+++ basic01.py
@@ -0,0 +1,44 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print '''line1
+line2
+line3
+'''
+line1
+line2
+line3
+
+>>> true
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    true
+NameError: name 'true' is not defined
+>>> True
+True
+>>> False
+False
+>>> 3>2
+True
+>>> Ture and False
+
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    Ture and False
+NameError: name 'Ture' is not defined
+>>> True and False
+False
+>>> False and False
+False
+>>> False or True
+True
+>>> not True
+False
+>>> if age > 18;
+SyntaxError: invalid syntax
+>>> if age >18
+SyntaxError: invalid syntax
+>>> mystring = 'hello world!'
+>>> print(mystring)
+hello world!
+>>> 
---
+++ hello.py
@@ -0,0 +1,2 @@
+name = raw_input('enter your name:')
+print 'hello',name
