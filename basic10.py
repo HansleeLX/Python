4f8cc148d949bc4a42882fef0eaa99426cbff967
hanslee <hanslee.lx@gmail.com>
2018-07-27 11:05:54 +1000

    basic functions
    the basic functions

---
+++ basic10.py
@@ -0,0 +1,9 @@
+a = int(raw_input('enter the first number: '))
+b = int(raw_input('enter the second number: '))
+cmp(a,b)
+if a > b:
+    print "first number is larger"
+elif a == b:
+    print "they are equal"
+else:
+    print "second number is larger"
