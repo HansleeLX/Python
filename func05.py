d734391f493fb3bf364447e2127c45bdb9d6e91e
hanslee <hanslee.lx@gmail.com>
2018-08-09 22:30:57 +1000

    跨文件调用
    跨文件调用

---
+++ func05.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, '..\GitHub\testpython')
+from testpython import functest
+
+import func02
+
+def func05():
+    a = int( input("please input the parameter A: "))
+    b = int( input("please input the parameter B: "))
+    c = int( input("please input the parameter C: "))
+    #func02.func02(a,b,c)
+    #func031()
+    functest.functest()
+    return
