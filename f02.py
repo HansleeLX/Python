0057d04b2d348d8a6ef28aa9d7be8ae32fcb0020
hanslee <hanslee.lx@gmail.com>
2018-08-16 11:03:08 +1000

    切片&列表表达式
    列表表达式，切片等

---
+++ f02.py
@@ -0,0 +1,19 @@
+def f02():
+    L = []
+    n = 1
+    j = 0
+    while n <= 99:
+        L.append(n)
+        n = n + 2
+        j = j + 1
+    return j
+
+def f021(a,b):
+    N = [a,b]
+    print N
+
+
+def f021(a,b,c=1):
+    M = [a,b,c]
+    print M
+    
